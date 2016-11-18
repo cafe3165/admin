112aea019b6bbdab34680c1f8c5a3eacdeccd065
Joseph <78599297@qq.com>
2016-11-19 03:20:37 +0800

    change com_history/head/find.xml

---
+++ app/src/main/java/com/test/admin/promulgator/ActInfomation.java
@@ -0,0 +1,15 @@
+package com.test.admin.promulgator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.test.admin.R;
+
+public class ActInfomation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_infomation);
+    }
+}
