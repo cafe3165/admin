c1f0add92959c047c223d2369a025eb561ee3662
Joseph <78599297@qq.com>
2016-12-11 19:39:11 +0800

    修改参与者侧滑框，增加了个人中心与设置

--- app/src/main/java/com/test/admin/Participant/newMain.java
+++ app/src/main/java/com/test/admin/Participant/newMain.java
@@ -7,6 +7,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.View;
 
+import com.test.admin.PaticipantFragment.PersonData;
+import com.test.admin.PaticipantFragment.Setting;
 import com.test.admin.R;
 import com.test.admin.PaticipantFragment.Head;//////
 import com.test.admin.fragment.FragmentSetting;
@@ -19,15 +21,15 @@ public class newMain extends BaseActivity implements View.OnClickListener {
     public static final String TAG_ONE = "one";
     public static final String TAG_TWO = "two";
     public static final String TAG_THREE = "three";
-    public static final String TAG_FOUR = "four";
+    public static final String TAG_FOUR = "four";/////
     public static final String TAG_SETTING = "setting";
     private DrawerLayout mDrawerLayout;
     private FragmentManager mFragmentManager;
     private Head mFragmentOne;
-    private FragmentTwo mFragmentTwo;
+    private PersonData mFragmentTwo;
     private FragmentThree mFragmentThree;
 
-    private FragmentSetting mFragmentSetting;
+    private Setting mFragmentSetting;///////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +65,7 @@ public class newMain extends BaseActivity implements View.OnClickListener {
         mFragmentManager = this.getSupportFragmentManager();
         if (savedInstanceState != null) {
             mFragmentOne = (Head) mFragmentManager.findFragmentByTag(TAG_ONE);
-            mFragmentTwo = (FragmentTwo) mFragmentManager.findFragmentByTag(TAG_TWO);
+            mFragmentTwo = (PersonData) mFragmentManager.findFragmentByTag(TAG_TWO);///////////////
             mFragmentThree = (FragmentThree) mFragmentManager.findFragmentByTag(TAG_THREE);
         }
         if (mFragmentOne == null) {
@@ -72,7 +74,7 @@ public class newMain extends BaseActivity implements View.OnClickListener {
                     .add(R.id.vf_container, mFragmentOne, TAG_ONE).commit();
         }
         if (mFragmentTwo == null) {
-            mFragmentTwo = new FragmentTwo();
+            mFragmentTwo = new PersonData();
             mFragmentManager.beginTransaction()
                     .add(R.id.vf_container, mFragmentTwo, TAG_TWO).commit();
         }
@@ -89,7 +91,7 @@ public class newMain extends BaseActivity implements View.OnClickListener {
 //        }
 
         if (mFragmentSetting == null) {
-            mFragmentSetting = new FragmentSetting();
+            mFragmentSetting = new Setting();
             mFragmentManager.beginTransaction()
                     .add(R.id.vf_container, mFragmentSetting, TAG_SETTING).commit();
         }
@@ -102,14 +104,24 @@ public class newMain extends BaseActivity implements View.OnClickListener {
 
         if (tag.equals(TAG_ONE)) {
             mFragmentManager.beginTransaction().show(mFragmentOne).hide(mFragmentTwo).hide(mFragmentThree).hide(mFragmentSetting).commit();
-            setCustomTitle("活动审核", true);
+            setCustomTitle("首页", true);
         } else if (tag.equals(TAG_TWO)) {
             mFragmentManager.beginTransaction().hide(mFragmentOne).show(mFragmentTwo).hide(mFragmentThree).hide(mFragmentSetting).commit();
-            setCustomTitle("权限审核", true);
+            setCustomTitle("个人中心", true);
         } else if (tag.equals(TAG_THREE)) {
             mFragmentManager.beginTransaction().hide(mFragmentOne).hide(mFragmentTwo).show(mFragmentThree).hide(mFragmentSetting).commit();
-            setCustomTitle("用户信息更改", true);
-        }else if (tag.equals(TAG_SETTING)) {
+            setCustomTitle("活动查找", true);
+
+        }
+
+        else if (tag.equals(TAG_FOUR)) {
+            mFragmentManager.beginTransaction().hide(mFragmentOne).hide(mFragmentTwo).show(mFragmentThree).hide(mFragmentSetting).commit();
+            setCustomTitle("通知", true);
+
+
+        }
+
+        else if (tag.equals(TAG_SETTING)) {
             mFragmentManager.beginTransaction().hide(mFragmentOne).hide(mFragmentTwo).show(mFragmentThree).show(mFragmentSetting).commit();
             setCustomTitle("设置", true);
         }
@@ -128,10 +140,10 @@ public class newMain extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_01:
+            case R.id.btn_head:
                 showContent(TAG_ONE);
                 break;
-            case R.id.btn_02:
+            case R.id.btn_person:
                 showContent(TAG_TWO);
                 break;
             case R.id.btn_03:
