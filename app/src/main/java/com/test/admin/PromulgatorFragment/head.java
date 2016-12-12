3caf9927542bcd2df8f2e046bb3cbc5aaa36c5c4
Joseph <78599297@qq.com>
2016-12-12 22:22:56 +0800

    修改侧滑框和添加侧滑框头

---
+++ app/src/main/java/com/test/admin/PromulgatorFragment/head.java
@@ -0,0 +1,53 @@
+package com.test.admin.PromulgatorFragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.test.admin.R;
+import com.test.admin.activity.ActivityDetail;
+import com.test.admin.adapter.AcApplyAdapter;
+import com.test.admin.bean.AsAcApplying;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+
+import static com.test.admin.bean.Parameters.staticObjectdId;
+
+/**
+ * Created by hc6 on 2016/11/16.
+ */
+
+public class head extends Fragment {
+
+    private ListView lv_activity;
+    private AcApplyAdapter mAdapter;
+    private List<AsAcApplying> asActivityList = new ArrayList<AsAcApplying>();
+    public head() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_head, container, false);
+
+
+
+        return view;
+    }
+
+}
+
+
+
