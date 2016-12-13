d93a469873f2415a5674ec54d741a6e087397620
Notgg <1937270244@qq.com>
2016-12-13 13:14:37 +0800

    Merge branch 'master' of https://github.com/cafe3165/admin into wu
    # Conflicts:
    #	.idea/misc.xml

---
+++ app/src/main/java/com/test/admin/newLogin/identityActivity.java
@@ -0,0 +1,18 @@
+package com.test.admin.newLogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.test.admin.R;
+
+public class identityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.identity);
+        ImageButton btn1 = (ImageButton) findViewById(R.id.imaBtn1);
+        //btn1.setOnClickListener(new );
+    }
+}
