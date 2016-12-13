e1394c156ef4a05958c199c6034e6be6dc6d947d
lijk365421 <834303575@qq.com>
2016-12-14 02:00:39 +0800

    参与者通知列表查询Activity更新重写

--- app/src/main/java/com/test/admin/PaticipantFragment/TongZhi.java
+++ app/src/main/java/com/test/admin/PaticipantFragment/TongZhi.java
@@ -1,18 +1,38 @@
 package com.test.admin.PaticipantFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.test.admin.Participant.ImformViewActivity;
 import com.test.admin.R;
+import com.test.admin.adapter.ImformationAdapter;
+import com.test.admin.bean.AsImformation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+
+import static com.test.admin.bean.Parameters.staticObjectdId;
 
 /**
  * Created by hc6 on 2016/11/16.
  */
 
 public class TongZhi extends Fragment {
+
+    private ListView lv_imformation;
+    private ImformationAdapter myImformationAdapter;
+    private List<AsImformation> asImformationList = new ArrayList<AsImformation>();
+
     public TongZhi() {
         // Required empty public constructor
     }
@@ -20,7 +40,36 @@ public class TongZhi extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.com_activity_no_find, container, false);
+        View view = inflater.inflate(R.layout.com_activity_no_find, container, false);
+
+        lv_imformation = (ListView)view.findViewById(R.id.itemList);
+
+        BmobQuery<AsImformation> bmobQuery = new BmobQuery<AsImformation>();
+        bmobQuery.addQueryKeys("objectId,imTitle,imContent");
+        bmobQuery.findObjects(new FindListener<AsImformation>() {
+            @Override
+            public void done(List<AsImformation> list, BmobException e) {
+
+                if (e == null) {
+                    asImformationList.addAll(list);
+
+                    myImformationAdapter = new ImformationAdapter(getActivity(),asImformationList);
+
+                    lv_imformation.setAdapter(myImformationAdapter);
+                }
+            }
+        });
+
+        lv_imformation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getActivity(), ImformViewActivity.class));
+
+                AsImformation asImformation  = asImformationList.get(position);
+                staticObjectdId = asImformation.getObjectId().toString();
+            }
+        });
+        return view;
     }
 
 }
