e09954943868c02ab74f0f4cd2d00c983b457adf
lijk365421 <834303575@qq.com>
2016-12-14 18:32:13 +0800

    初步完成管理员权限审核后发送邮件功能代码

--- app/src/main/java/com/test/admin/model/Function.java
+++ app/src/main/java/com/test/admin/model/Function.java
@@ -2,7 +2,15 @@ package com.test.admin.model;
 
 import android.widget.Toast;
 
+import com.test.admin.bean.AsPromulgator;
+
 import java.util.List;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 
 import static cn.bmob.v3.Bmob.getApplicationContext;
 
@@ -12,13 +20,11 @@ import static cn.bmob.v3.Bmob.getApplicationContext;
 
 public class Function {
 
-    public static final int UPDATE_TEXT = 1;
-
     public static void showToast(String msg){
 
         Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_SHORT).show();
     }
-
+    //判断输入是否为空
     public static boolean judge(List<String> list){
 
         boolean flag = true;
@@ -32,5 +38,23 @@ public class Function {
             }
         }
         return  flag;
+    }
+    //发送邮件
+    public void sendEmial(AsPromulgator asPromulgator){
+
+        final String proEmail = asPromulgator.getEmail();
+        BmobUser.requestEmailVerify(proEmail, new UpdateListener() {
+            @Override
+            public void done(BmobException e) {
+
+                if(e == null){
+
+                }else {
+
+                    showToast("发送邮件操作失败" + "\t" + e.getErrorCode() + ":" + e.getMessage());
+                }
+            }
+        });
+
     }
 }
