4da583a04163727ed89c80a47f12ed4fd5670d36
Notgg <1937270244@qq.com>
2016-12-14 21:39:46 +0800

    注册步骤3

---
+++ app/src/main/java/com/test/admin/newLogin/register3_1Activity.java
@@ -0,0 +1,18 @@
+package com.test.admin.newLogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.test.admin.R;
+
+public class register3_1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_step3_1);
+    }
+
+}
