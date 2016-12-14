b80d8f63d8255b5f10a91adb76f3fd7e1054634d
TraygeShaw <540524095@qq.com>
2016-12-14 20:45:29 +0800

    修改设置背景，三个身份的设置分开

---
+++ app/src/main/java/com/test/admin/PromulgatorFragment/FaBuZheSetting.java
@@ -0,0 +1,26 @@
+package com.test.admin.PromulgatorFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.test.admin.R;
+
+/**
+ * Created by hc6 on 2016/11/16.
+ */
+
+public class FaBuZheSetting extends Fragment {
+    public FaBuZheSetting() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_setting22, container, false);
+    }
+
+}
