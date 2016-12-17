9e51acadcfcf253ade0cc42afd6be50fd9c5e9e6
lijk365421 <834303575@qq.com>
2016-12-17 12:54:17 +0800

    报名功能完善--取消报名

--- app/src/main/java/com/test/admin/model/AsAppForm.java
+++ app/src/main/java/com/test/admin/model/AsAppForm.java
@@ -63,7 +63,8 @@ public class AsAppForm {
 
                             showToast("报名成功");
                             //更改按钮状态
-                            apply.setText("报名成功");
+                            apply.setText("取消报名");
+                            apply.setEnabled(true);
                             //将用户报名的活动添加到用户已报名的活动数组
                             BmobQuery<AsParticipant> query = new BmobQuery<AsParticipant>();
                             query.addWhereEqualTo("objectId",parObjectdId);
@@ -97,22 +98,66 @@ public class AsAppForm {
     public void acParCancleApply(final Button apply,final String acObjectdId, final String parObjectdId){
 
         //获取当前活动对应的报名表
-        BmobQuery<AsApplicationForm> query = new BmobQuery<AsApplicationForm>();
-        query.addWhereEqualTo("apParId",acObjectdId);
+        BmobQuery<AsApplicationForm> query = new BmobQuery<>();
+        query.addWhereEqualTo("apAcId",acObjectdId);
         query.findObjects(new FindListener<AsApplicationForm>() {
             @Override
             public void done(List<AsApplicationForm> list, BmobException e) {
 
-                //获取当前用户在报名表报名的参与者数组所在的位置
-                List<String> list1 = new ArrayList<String>();
-                list1.addAll(list.get(0).getApParId());
-                int index = list1.indexOf(parObjectdId);
-                //获取当前报名表的参与者状态数组并删除指定位置的值
-                List<String> list2 = new ArrayList<String>();
-                list2.addAll(list.get(0).getApParStatus());
-                list2.remove(index);
+                if(e == null){
+
+                    //showToast("操作成功");
+                    //获取当前用户在报名表报名的参与者数组所在的位置
+                    List<String> list1 = new ArrayList<String>();
+                    list1.addAll(list.get(0).getApParId());
+                    int index = list1.indexOf(parObjectdId);
+                    //获取当前报名表的参与者状态数组并删除指定位置的值
+                    List<String> list2 = new ArrayList<String>();
+                    list2.addAll(list.get(0).getApParStatus());
+                    list2.remove(index);
+                    //showToast("操作成功" + index + list.get(0).getApParStatus().get(index));
+                    list.get(0).setApParStatus(list2);
+                    list.get(0).removeAll("apParId", Arrays.asList(parObjectdId));
+                    list.get(0).update(new UpdateListener() {
+                        @Override
+                        public void done(BmobException e) {
+
+                            if(e == null){
+                                showToast("取消报名成功");
+                                //更改按钮状态和text
+                                apply.setText("报名");
+                                apply.setEnabled(true);
+                                //将用户报名的活动Id从用户已报名的活动数组删除
+                                BmobQuery<AsParticipant> query = new BmobQuery<AsParticipant>();
+                                query.addWhereEqualTo("objectId",parObjectdId);
+                                query.findObjects(new FindListener<AsParticipant>() {
+                                    @Override
+                                    public void done(List<AsParticipant> list, BmobException e) {
+
+                                        if (e == null) {
+                                            list.get(0).removeAll("parAcId", Arrays.asList(acObjectdId));
+                                            list.get(0).update(new UpdateListener() {
+                                                @Override
+                                                public void done(BmobException e) {
+
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }else{
+                                showToast("操作失败" + "\t" + e.getErrorCode() + ":" + e.getMessage());
+                                //更改按钮状态和text
+                                apply.setEnabled(true);
+                            }
+                        }
+                    });
+                }else{
+                    showToast("操作失败" + "\t" + e.getErrorCode() + ":" + e.getMessage());
+                }
+
                 //将当前用户的Id从报名表中删除
-                list.get(0).setApParStatus(list2);
+                /*list.get(0).setApParStatus(list2);
                 list.get(0).removeAll("apParId", Arrays.asList(parObjectdId));
                 list.get(0).update(new UpdateListener() {
                     @Override
@@ -147,7 +192,7 @@ public class AsAppForm {
                             apply.setEnabled(true);
                         }
                     }
-                });
+                });*/
             }
         });
     }
