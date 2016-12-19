8b75c00059c896d4c5c67bae568328b564d49b8a
lijk365421 <834303575@qq.com>
2016-12-20 02:16:31 +0800

    发布新活动界面更新后功能完善

--- app/src/main/java/com/test/admin/promulgator/newNotice.java
+++ app/src/main/java/com/test/admin/promulgator/newNotice.java
@@ -2,8 +2,11 @@ package com.test.admin.promulgator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.test.admin.R;
 import com.test.admin.bean.AsImformation;
@@ -18,20 +21,41 @@ public class newNotice extends AppCompatActivity {
     private EditText imOrganizer;
     private EditText imContent;
     private EditText imAudiences;
-    private EditText imPushScope;
+    private Spinner imPushScope_1;
+    private Spinner imPushScope_2;
     private Button publish;
+
+    String mImPushScope_1 = null;
+    String mImPushScope_2 = null;
+    String[] mCollege;
+    String[] mGrade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_notice);
 
+
+        String[] college = this.getResources().getStringArray(R.array.xueyuan);
+        String[] grade1 = this.getResources().getStringArray(R.array.grade);
+        ArrayAdapter<String> collegeAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,college);
+        ArrayAdapter<String> gradeAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,grade1);
+
+        mCollege = college;
+        mGrade = grade1;
+
         imTitle = (EditText)findViewById(R.id.imTitle);
-        //imOrganizer = (EditText)findViewById(R.id.imOrganizer);
+        imOrganizer = (EditText)findViewById(R.id.imOrganizer);
         imContent = (EditText)findViewById(R.id.imContent);
         imAudiences = (EditText)findViewById(R.id.imAudiences);
-        imPushScope = (EditText)findViewById(R.id.imPushScope);
+        imPushScope_1 = (Spinner)findViewById(R.id.imPushScope_1);
+        imPushScope_2 = (Spinner)findViewById(R.id.imPushScope_2);
         publish = (Button)findViewById(R.id.publish);
+
+        imPushScope_1.setAdapter(collegeAdapter);
+        imPushScope_2.setAdapter(gradeAdapter);
+        imPushScope_1.setOnItemSelectedListener(new onItemClickListener());
+        imPushScope_2.setOnItemSelectedListener(new onItemClickListener());
 
         publish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,17 +63,33 @@ public class newNotice extends AppCompatActivity {
                 //改变按钮状态为不可按
                 publish.setEnabled(false);
                 //获取EditText信息
-                String myImTitle = imTitle.getText().toString();
-                String myImOrganizer = imOrganizer.getText().toString();
-                String myImContent = imContent.getText().toString();
-                String myImAudiences = imAudiences.getText().toString();
-                String myImPushScope = imPushScope.getText().toString();
+                String mImTitle = imTitle.getText().toString();
+                String mImOrganizer = imOrganizer.getText().toString();
+                String mImContent = imContent.getText().toString();
+                String mImAudiences = imAudiences.getText().toString();
                 pObjectdId = (String) AsPromulgator.getObjectByKey("objectId");
                 //发布通知
                 AsImformationMethod asImformation = new AsImformationMethod();
-                asImformation.asImAdd(publish,myImTitle,myImOrganizer,myImContent,myImAudiences,myImPushScope,myImPushScope,pObjectdId);
+                asImformation.asImAdd(publish,mImTitle,mImOrganizer,mImContent,mImAudiences,mImPushScope_1,mImPushScope_2,pObjectdId);
             }
         });
+    }
+    //下拉框监听
+    class onItemClickListener implements Spinner.OnItemSelectedListener{
 
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3){
+
+            switch (arg0.getId()){
+                case R.id.imPushScope_1:
+                    mImPushScope_1 = mCollege[arg2];
+                    break;
+                case R.id.imPushScope_2:
+                    mImPushScope_2 = mGrade[arg2];
+                    break;
+            }
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
     }
 }
